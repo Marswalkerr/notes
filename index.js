8af967965545eb893276f37744706f06ce9b3f64
marswalkerr <sanketsavani004@gmail.com>
2024-06-22 14:15:29 +0530

    added note-update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const { fileLoader } = require("ejs");
 const app = express();
 
 app.set("view engine", "ejs");
@@ -32,6 +33,70 @@ app.get("/files/delete/:filename", (req, res) => {
         res.redirect("/");
     })
 })
+
+app.get("/edit/:filename", (req, res) => {
+    fs.readFile(`./files/${req.params.filename}`, "utf-8", (err, filedata) => {
+        res.render("edit", {filename: req.params.filename, filedata: filedata});
+    })
+})
+
+app.post("/update/:filename", (req, res) => {
+    const oldFileName = req.params.filename; // current filename
+    const newFileName = req.body.title.split(" ").join("") + ".txt"; // new filename from form input
+    const newFileDetails = req.body.details; // new details from form input
+
+    fs.readFile(`./files/${oldFileName}`, "utf-8", (err, filedata) => {
+        if (err) {
+            console.log(err);
+            res.send("Error reading file for update");
+            return;
+        }
+
+        // Check if details have changed
+        if (filedata !== newFileDetails) {
+            // Update file content
+            fs.writeFile(`./files/${newFileName}`, newFileDetails, (err) => {
+                if (err) {
+                    console.log(err);
+                    res.send("Error updating file details");
+                    return;
+                }
+
+                // Remove old file if filename has changed
+                if (oldFileName !== newFileName) {
+                    fs.unlink(`./files/${oldFileName}`, (err) => {
+                        if (err) {
+                            console.log(err);
+                            res.send("Error deleting old file");
+                            return;
+                        }
+                        console.log("Old file deleted successfully");
+                    });
+                }
+
+                console.log("File details updated successfully");
+                res.redirect("/"); // redirect to home page after update
+            });
+        } else {
+            // Only rename the file if the filename has changed
+            if (oldFileName !== newFileName) {
+                fs.rename(`./files/${oldFileName}`, `./files/${newFileName}`, (err) => {
+                    if (err) {
+                        console.log(err);
+                        res.send("Error renaming file");
+                        return;
+                    }
+                    console.log("File renamed successfully");
+                    res.redirect("/"); // redirect to home page after rename
+                });
+            } else {
+                console.log("No changes detected");
+                res.redirect("/"); // redirect to home page if no changes detected
+            }
+        }
+    });
+});
+
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
